a4d35b5e955b88ab2331ba3219b0fcb6b64369eb
meghna-khanna19 <91715529+meghna-khanna19@users.noreply.github.com>
2021-10-01 15:24:06 +1000

    Create scatter.js

---
+++ scatter.js
@@ -0,0 +1,4 @@
+var vg_1= "scatter.json";
+vegaEmbed("#bar_chart", vg_1).then(function(_result) { 
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
